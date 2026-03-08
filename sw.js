a0ca4f02031c2176c0880b060ff6aae621393968
Novogarden <contact@novogardenhub.com>
2026-03-08 11:39:28 +0100

    SW v3: vide tout le cache

--- sw.js
+++ sw.js
@@ -1,24 +1,9 @@
-const CACHE_NAME = 'novogarden-v2';
-const ASSETS = [
-  '/novogarden-app/',
-  '/novogarden-app/index.html',
-  '/novogarden-app/manifest.json'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
+const CACHE = 'ng-v3';
+self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(
-    keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-  )));
+  e.waitUntil(caches.keys().then(k => Promise.all(k.map(n => caches.delete(n)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
