a5c5a051636570ba59bd99f3e40721510c1fc00f
Novogarden <contact@novogardenhub.com>
2026-09-03 18:23:43 +0200

    Cache ng-v8: diffusion de la page partenaire et du lien d inscription

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ng-v7';
+const CACHE = 'ng-v8';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(k => Promise.all(k.map(n => caches.delete(n)))));
