8d7814f46e75f71dc6a39a037db992288ee96e91
Novogarden <contact@novogardenhub.com>
2026-09-06 16:18:29 +0200

    Cache ng-v19

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ng-v18';
+const CACHE = 'ng-v19';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(k => Promise.all(k.map(n => caches.delete(n)))));
