bdc5f5624b88979d68e7acd98c822af9c2cd7983
Novogarden <contact@novogardenhub.com>
2026-09-13 21:10:13 +0200

    Cache ng-v27

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ng-v26';
+const CACHE = 'ng-v27';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(k => Promise.all(k.map(n => caches.delete(n)))));
