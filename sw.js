bb225326e64de54290c4f9958008205466b2e92b
Novogarden <contact@novogardenhub.com>
2026-09-14 14:25:37 +0200

    Version 38 : forcer la mise a jour des nouveaux modules sur mobile

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ng-v37';
+const CACHE = 'ng-v38';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(k => Promise.all(k.map(n => caches.delete(n)))));
